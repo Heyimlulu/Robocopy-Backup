d97b14fc390760cd14e0e0675af74712b76c0262
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-10-14 20:30:20 +0200

    Update script with mail notification

---
+++ PasswordEncryption.ps1
@@ -0,0 +1,8 @@
+﻿function passwordEncryption () {
+    $secureStringPwd = Read-Host -AsSecureString
+    $secureStringText = $secureStringPwd | ConvertFrom-SecureString
+    Set-Content "C:\temp\EncryptedPassword.txt" $secureStringText
+}
+
+# Simply run the function
+passwordEncryption
